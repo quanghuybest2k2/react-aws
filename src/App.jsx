b331b59b43112dd5e1b97a10a05514bc546c4fe7
Đoàn Quang Huy <82820145+quanghuybest2k2@users.noreply.github.com>
2024-11-29 15:48:46 +0700

    set up

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get("http://3.27.216.224/users")
+      .then((response) => {
+        setUsers(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching users:", error);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className="container mt-5">
+      <h1 className="mb-4 text-center">Danh sách người dùng</h1>
+      {loading ? (
+        <div className="text-center">
+          <div className="spinner-border text-primary" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        </div>
+      ) : (
+        <table className="table table-striped table-responsive">
+          <thead className="table-dark">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Họ và tên</th>
+              <th scope="col">Email</th>
+              <th scope="col">Ngày tạo</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user.id}>
+                <th scope="row">{user.id}</th>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{new Date(user.created_at).toLocaleDateString()}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+}
+
+export default App;
